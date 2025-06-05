752842820a1739fd0f2c6c63669f3d7c43d1c60f
jimdevGR <karagiannisj01@gmail.com>
2025-06-05 13:54:20 +0300

    Add files via upload

--- Q4_sql_parquet.py
+++ Q4_sql_parquet.py
@@ -1,5 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import *
 from pyspark.sql.functions import col
 
 # Αρχικοποίηση spark session.
@@ -16,29 +15,6 @@ my_spark_context.setLogLevel("ERROR")
 
 input_file_path_2024 = "hdfs://hdfs-namenode:9000/user/dikaragiannis/data/parquet/yellow_tripdata_2024"
 output_dir_path = "hdfs://hdfs-namenode:9000/user/dikaragiannis/project_2025_outputs"
-
-yellow_tripdata_schema = StructType([
-    StructField(name = "VendorID", dataType = IntegerType()),
-    StructField(name = "tpep_pickup_datetime", dataType = TimestampType()),
-    StructField(name = "tpep_dropoff_datetime", dataType = TimestampType()),
-    StructField(name = "passenger_count", dataType = IntegerType()),
-    StructField(name = "trip_distance", dataType = DoubleType()),
-    StructField(name = "RatecodeID", dataType = IntegerType()),
-    StructField(name = "store_and_fwd_flag", dataType = StringType()),
-    StructField(name = "PULocationID", dataType = IntegerType()),
-    StructField(name = "DOLocationID", dataType = IntegerType()),
-    StructField(name = "payment_type", dataType = IntegerType()),
-    StructField(name = "fare_amount", dataType = DoubleType()),
-    StructField(name = "extra", dataType = DoubleType()),
-    StructField(name = "mta_tax", dataType = DoubleType()),
-    StructField(name = "tip_amount", dataType = DoubleType()),
-    StructField(name = "tolls_amount", dataType = DoubleType()),
-    StructField(name = "improvement_surcharge", dataType = DoubleType()),
-    StructField(name = "total_amount", dataType = DoubleType()),
-    StructField(name = "congestion_surcharge", dataType = DoubleType()),
-    StructField(name = "airport_fee", dataType = DoubleType()),
-    StructField(name = "cbd_congestion_fee", dataType = DoubleType())
-])
 
 tripdata_df = my_spark_session.read.parquet(input_file_path_2024).select(col("tpep_pickup_datetime"), col("VendorID"))
 
